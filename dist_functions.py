4cd8ba2f33162ac6f9f878b8f889091fd1ae2592
shaiTheKimhi <shaikimhi221199@gmail.com>
2020-12-07 17:32:36 +0200

    working with test

--- dist_functions.py
+++ dist_functions.py
@@ -13,8 +13,8 @@ def dist_cpu(A, B, p):
     res = 0.0
     for i in range(A.shape[0]):
         for j in range(A.shape[1]):
-            res += abs(A[i][j] - B[i][j])**p    
-    res = res ** (1/float(p))
+            res += abs(A[i][j] - B[i][j])**p
+    res = res ** (1/p)
     return res
 
 
@@ -30,10 +30,8 @@ def dist_numba(A, B, p):
     res = 0.0
     for i in prange(A.shape[0]):
         for j in prange(A.shape[1]):
-            #calculate difference for the i,j element, power by p and sum up
             res += abs(A[i][j] - B[i][j])**p
-    
-    res = res ** (1/float(p))
+    res = res ** (1/p)
     return res
 	
 
@@ -44,46 +42,39 @@ def dist_gpu(A, B, p):
      np.array
          p-dist between A and B
      """
-    
     C = np.arange(1)
     C[0] = 0.0
     res = 0.0
-    #copy memory arrays to GPU kernel
     C = cuda.to_device(C)
     A = cuda.to_device(A)
     B = cuda.to_device(B)
-    #run kernel
     dist_kernel[1000, 1000](A, B, p, C)
-    #copy back
     C = C.copy_to_host()
-    #calculate root
-    res = C[0] ** (1/float(p))
+    res = C[0] ** (1/p)
+    return res
 
 @cuda.jit
 def dist_kernel(A, B, p, C):
-    #get indices
     i = cuda.threadIdx.x
     j = cuda.blockIdx.x
-    #if thread is not out-of-bound calculate differences for indices j,i and store in global memory
     if i < A.shape[0] and j < A.shape[1]:
-        res = abs(A[j][i] - B[j][i])**p 
-        cuda.atomic.add(C, 0, res) 
+        res = abs(A[i][j] - B[i][j])**p
+        cuda.atomic.add(C, 0, res)  # need for every thread to sum near threads
+    
    
 #this is the comparison function - keep it as it is.
 def dist_comparison():
     A = np.random.randint(0,256,(1000, 1000))
     B = np.random.randint(0,256,(1000, 1000))
-    p = [1, 2]
+#    p = [1, 2]
+    p = range(76,80)
 
     def timer(f, q):
         return min(timeit.Timer(lambda: f(A, B, q)).repeat(3, 20))
-    
-    
-    
-    
+
     for power in p:
         print('p=' + str(power))
-        print('     [*] CPU:', timer(dist_cpu,power))
+        #print('     [*] CPU:', timer(dist_cpu,power))
         print('     [*] Numba:', timer(dist_numba,power))
         print('     [*] CUDA:', timer(dist_gpu, power))
 
